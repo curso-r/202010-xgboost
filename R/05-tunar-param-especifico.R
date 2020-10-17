c45879f90fa8a2dbba6a029b2b8566ad073a1837
Athos Damiani <athos.damiani@gmail.com>
2020-10-17 08:37:40 -0300

    asdas

--- R/05-tunar-param-especifico.R
+++ R/05-tunar-param-especifico.R
@@ -3,27 +3,37 @@ library(tidymodels)
 library(modeldata)
 data("lending_club")
 
-model <- boost_tree(mtry = tune(), trees = 100, min_n = 30, 
-                    tree_depth = 4, learn_rate = 0.1, loss_reduction = 0,
-                    sample_size = 0.8)  %>% 
-  set_engine("xgboost", lambda = tune("lambda")) %>% 
+model <- boost_tree(
+  mtry = tune(),
+  trees = 100,
+  min_n = 30,
+  tree_depth = 4,
+  learn_rate = 0.1,
+  loss_reduction = 0,
+  sample_size = 0.8
+)  %>%
+  set_engine("xgboost", lambda = tune("lambda")) %>%
   set_mode("classification")
 
-
-rec <- recipe(Class ~ ., data = lending_club) %>% 
+rec <- recipe(Class ~ ., data = lending_club) %>%
   step_dummy(all_nominal(), -all_outcomes())
 
-
-pars <- model %>% 
-  parameters() %>% 
+pars <- model %>%
+  parameters() %>%
   update(
     lambda = dials::penalty()
   )
 
+grid_de_params <- expand.grid(mtry = c(0.5, 0.8, 0.9), lambda = c(0.1, 0, 1e6))
+
 tun <- tune_grid(
-  model, 
+  model,
   preprocessor = rec,
   resamples = vfold_cv(lending_club, 5),
-  grid = expand.grid(mtry = c(0.5, 0.8, 0.9), lambda = c(0.1, 0, 1e6)),
+  grid = grid_de_params,
   param_info = pars
 )
+
+autoplot(tun)
+show_best(tun)
+select_best(tun)
